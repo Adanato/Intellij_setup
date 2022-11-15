5b374f8b7c9c80cbf9807722ab96a55eecaa3eda
Adanato <AdanatoNw@gmail.com>
2022-11-15 09:36:59 -0500

    Startup learning intellij

---
+++ src/Product.java
@@ -0,0 +1,25 @@
+public class Product {
+
+    private String name;
+
+    public Product(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the product.
+     * @param name The name of the product
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+
+
+
---
+++ src/ProductTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+    @org.junit.jupiter.api.Test
+    void getName() {
+        assertThat(1 > 2).isTrue();
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+}
